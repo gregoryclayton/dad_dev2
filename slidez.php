5dd8e54c8c1c9db2a8afdbc0dea8f5348a34fe3a
density <44949218+gregoryclayton@users.noreply.github.com>
2025-10-18 17:24:18 -0400

    Create slidez.php

---
+++ slidez.php
@@ -0,0 +1,23 @@
+<?php
+// --- SLIDESHOW IMAGES FROM pusers/*/work ---
+$images = [];
+$pusersDir = __DIR__ . '/pusers';
+
+if (is_dir($pusersDir)) {
+    $userFolders = scandir($pusersDir);
+    foreach ($userFolders as $userFolder) {
+        if ($userFolder === '.' || $userFolder === '..') continue;
+        $workDir = $pusersDir . '/' . $userFolder . '/work';
+        if (is_dir($workDir)) {
+            $workFiles = scandir($workDir);
+            foreach ($workFiles as $file) {
+                if (preg_match('/\.(jpg|jpeg|png|gif)$/i', $file)) {
+                    $images[] = 'p-users/' . $userFolder . '/work/' . $file;
+                }
+            }
+        }
+    }
+}
+// Randomize the order of images
+shuffle($images);
+?>
